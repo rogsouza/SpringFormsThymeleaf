c1d245225788cf3b08871a55353d4e8dd455b476
Rogério Souza <radsouza@localhost.localdomain>
2019-10-18 16:20:59 -0300

    first commit

---
+++ src/main/java/com/example/SpringFormsThymeleaf/SpringFormsThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringFormsThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormsThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormsThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringFormsThymeleaf/SpringFormsThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.SpringFormsThymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFormsThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
